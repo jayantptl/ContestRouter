d79066b3e64a57d5b64841c5de7e96cf05df73f3
Jayant Patel <96767549+jayantptl@users.noreply.github.com>
2022-11-26 13:46:58 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -5,7 +5,10 @@ const mongoose = require('mongoose');
 const authRoutes = require('./routes/authRoutes');
 const { requireAuth, checkUser } = require('./middleware/authMiddleware');
 const User = require('./models/User');
-// const Reminder=require('./models/User');
+
+const fetch = (url) =>
+  import("node-fetch").then(({ default: fetch }) => fetch(url));
+
 
 // cookies
 const cookieParser = require('cookie-parser');
@@ -14,8 +17,9 @@ app.use(cookieParser());
 
 
 // set the view engine to ejs
+app.set("views", __dirname + "/views"); //#
 app.set('view engine', 'ejs');
-app.use(express.static('public'));
+app.use(express.static(__dirname + "/public")); 
 app.use(express.json());
 app.use(express.urlencoded());
 app.use(authRoutes);
